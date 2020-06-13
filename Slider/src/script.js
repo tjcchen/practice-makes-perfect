7375ba84f12f5111adf75a60cd87ca4ec8326879
Andy Chen <chenyangpoppin@hotmail.com>
2020-06-13 21:53:59 +0800

    Add scaffolding for slider component

---
+++ Slider/src/script.js
@@ -0,0 +1,12 @@
+/**
+ * Slider Component - Javascript
+ * 
+ * @Date 2020-06-13
+ */
+
+;(function(win, undefined) {
+  'use strict';
+
+  console.log('print out something to the console');
+
+})(window);
