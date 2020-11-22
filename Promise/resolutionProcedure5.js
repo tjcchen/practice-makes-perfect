085e11cf9f8a083521aa2700bb93d69fa925dda6
Andy Chen <chenyangpoppin@hotmail.com>
2020-11-22 18:14:44 +0800

    feat: add throw error case

---
+++ Promise/resolutionProcedure5.js
@@ -0,0 +1,30 @@
+;(function() {
+  'use strict';
+
+  const promise1 = new Promise((resolve, reject) => {
+    reject('Outer Error');
+  });
+
+  // If x is an object or function,
+  // when retrieving the property x.then results in a thrown exception e, reject promise with e as the reason
+  const promise2 = promise1.then(
+    null,
+    () => {
+      // ... some code logic
+
+      throw new Error('Inner Error');
+
+      // ...
+    }
+  );
+  
+  // promise2 rejected: Error: Inner Error
+  promise2.then(
+    () => {
+      console.log('promise2 fulfilled');
+    },
+    reason => {
+      console.log('promise2 rejected: ' + reason);
+    }
+  );
+})();
