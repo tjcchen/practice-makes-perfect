6072c9d93a050160d90209e9c34080095f6a0303
Andy Chen <chenyangpoppin@hotmail.com>
2020-11-22 11:12:04 +0800

    feat: add onFulfilled not function case

---
+++ Promise/fulfilledPromise.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  const promise1 = new Promise((resolve, reject) => {
+    resolve('SUCCESS');
+  });
+
+  // If onFulfilled is not a function and promise1 is fulfilled, promise2 must be fulfilled with the same value as promise1
+  const promise2 = promise1.then(null, () => {
+    console.log('promise1 rejected');
+  });
+
+  // print out 'promise2 fulfilled: SUCCESS'
+  promise2.then(
+    value => {
+      console.log('promise2 fulfilled: ' + value);
+    },
+    () => {
+      console.log('promise2 rejected');
+    }
+  );
+})();
