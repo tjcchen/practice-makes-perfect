176ee56ddb2e98f1df25e20b410eb393e962753b
Andy Chen <chenyangpoppin@hotmail.com>
2020-11-24 17:11:54 +0800

    feat: add promise.all error catching mechanism

--- Promise/promiseAll3.js
+++ Promise/promiseAll3.js
@@ -6,15 +6,18 @@
 
   // promise only remember current promise status
   // this workaround used to solve due to promise2 rejected, Promise.all cannot be executed properly problem
-  Promise.all([promise1.catch(() => { 'failed' }), promise2.catch(() => { 'failed' })])
-         .then(
-           (msgs) => {
-             console.log('Promise.all fulfilled');
-             console.log(msgs); // [ undefined, undefined ]
-           }
-         ).catch(
-           () => {
-             console.log('Promise.all rejected');
-           }
-         );
+  Promise.all([
+    promise1.catch(() => { return 'promise1 failed'; }),
+    promise2.catch(() => { return 'promise2 failed'; })
+  ])
+  .then(
+    (msgs) => {
+      console.log('Promise.all fulfilled');
+      console.log(msgs); // [ undefined, 'promise2 failed' ]
+    }
+  ).catch(
+    () => {
+      console.log('Promise.all rejected');
+    }
+  );
 })();
