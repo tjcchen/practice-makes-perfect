c2b0f6a39b6ef94b393f480d0d969bfd500069bf
Andy Chen <chenyangpoppin@hotmail.com>
2020-11-26 16:33:59 +0800

    feat: add currying implementation phase 3

---
+++ Common/currying3.js
@@ -0,0 +1,23 @@
+function add(...args) {
+  let sum = 0;
+
+  function helper(...arguments) {
+    args = args.concat(arguments);
+
+    sum = args.reduce((accum, curr) => {
+      return accum + curr;
+    });
+
+    return helper;
+  }
+
+  helper.toString = function() {
+    return sum;
+  };
+
+  return helper;
+}
+
+const sum = add(1)(2, 3)(4, 5, 6)(7)(8)(9, 10);
+console.log(sum + 5); // 60
+console.log(sum.toString()); // 55
