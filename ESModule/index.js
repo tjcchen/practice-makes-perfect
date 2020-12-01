d9517d56fd6a690cbe7763e18e60660a7a685716
Andy Chen <chenyangpoppin@hotmail.com>
2020-12-01 17:21:35 +0800

    feat: add ESModule syntax example

--- ESModule/index.js
+++ ESModule/index.js
@@ -1,5 +1,13 @@
 import './moduleA.js';
 import moduleB from './moduleB.js';
+import moduleC from './moduleC.js';
+import { message } from './moduleC.js';                  // message is an export named 'message'
+import { message as customMessage } from './moduleC.js'; // rename message to customMessage
 
-console.log(moduleB);
+// import { text } from './moduleC.js'; // Cast Error, the requested module './moduleC.js' does not provide an export named 'text'
+
+console.log(moduleB);       // 1606814150132
+console.log(moduleC.text);  // Hello ESModule!, retrieve text property from default export object
+console.log(message);       // ESModule
+console.log(customMessage); // ESModule
 
---
+++ ESModule/moduleC.js
@@ -0,0 +1,5 @@
+export const message = 'ESModule';
+
+export default {
+  text: 'Hello ESModule!'
+};
