eaa1c37df1afd248b58cc529a171a2128bd4f806
Andy Chen <chenyangpoppin@hotmail.com>
2020-12-01 16:59:43 +0800

    feat: add cjs syntax example

--- CommonJS/index.js
+++ CommonJS/index.js
@@ -1,4 +1,8 @@
 require('./moduleA'); // print out timestamp coming from moduleA, 1606640540696
-const moduleB = require('./moduleB');
+const moduleB               = require('./moduleB');
+const { text }              = require('./moduleC'); // destructure text from moduleC
+const { text: anotherText } = require('./moduleC'); // rename text to anotherText
 
-console.log(moduleB); // print out timestamp coming from moduleB, 1606640540696
+console.log(moduleB);     // print out timestamp coming from moduleB, 1606640540696
+console.log(text);        // Hello CommonJS!
+console.log(anotherText); // Hello CommonJS!
