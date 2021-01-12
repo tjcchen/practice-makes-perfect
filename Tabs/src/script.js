c1365323e1a4fc093c91d99c2422ffd65ebb670b
Andy Chen <chenyangpoppin@hotmail.com>
2021-01-12 14:14:37 +0800

    refactor: variable rename thing

--- Tabs/src/script.js
+++ Tabs/src/script.js
@@ -14,17 +14,15 @@
     let oTabContents = doc.getElementsByClassName('tab-content');
     let activeClass  = 'active';
 
-    for (let i = 0, len = oTabs.length; i < len; i++) {
+    for (let i = 0, len1 = oTabs.length; i < len1; i++) {
       oTabs[i].addEventListener('click', function(e) {
-  
-        for (let j = 0, l = oTabs.length; j < l; j++) {
+        for (let j = 0, len2 = oTabs.length; j < len2; j++) {
           oTabs[j].classList.remove(activeClass);
           oTabContents[j].classList.remove(activeClass);
         }
 
         this.classList.add(activeClass);
         oTabContents[i].classList.add(activeClass);
-
       }, false);
     }
   };
